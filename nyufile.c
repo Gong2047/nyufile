0e89cd172f65b0a6289fe95b0b7e107b2d04b2d9
G <gg2484@nyu.edu>
2023-05-05 10:11:15 -0400

    milestone 4-6

--- nyufile.c
+++ nyufile.c
@@ -6,6 +6,9 @@
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <openssl/sha.h>
+#include <math.h>
 
 
 // Boot Sector
@@ -82,6 +85,9 @@ void print_bytes(char *addr, int size){
     printf("\n");
 }
 
+
+
+
 // Milestone 1: validate usage
 // There are several ways to invoke your nyufile program. Here is its usage:
 
@@ -109,6 +115,10 @@ void print_usage(){
     exit(EXIT_FAILURE);
 }
 
+
+
+
+
 // Milestone 2: print the file system information
 // If your nyufile program is invoked with option -i, it should print the following information about the FAT32 file system:
 
@@ -128,6 +138,10 @@ void print_system_info(int num_fats, int bytes_per_sector, int sectors_per_clust
     printf("Number of sectors per cluster = %d\n", sectors_per_cluster);
     printf("Number of reserved sectors = %d\n", reserved_sectors);
 }
+
+
+
+
 
 // Milestone 3: list the root directory
 // If your nyufile program is invoked with option -l, it should list all valid entries in the root directory with the following information:
@@ -177,7 +191,6 @@ char *process_name(char *filename, int is_dir){             // can be edited for
     return newName;
 }
 
-
 void list_root_directory(DirEntry *root_dir, int root_cluster, char* fat, char * data_region, int bytes_per_sector, int sectors_per_cluster){
     int i = 0;
     int total_entries = 0;
@@ -191,15 +204,15 @@ void list_root_directory(DirEntry *root_dir, int root_cluster, char* fat, char *
 
     while (root_dir[i].DIR_Name[0] != 0x00 || get_next_cluster(fat, current_cluster) != -1){
         
-        DirEntry* currentDir = &(root_dir[i]);
+        DirEntry* current_entry = &(root_dir[i]);
 
-        int isDir = currentDir->DIR_Attr & 0x10;
+        int isDir = current_entry->DIR_Attr & 0x10;
 
-        char* name = process_name(currentDir->DIR_Name, isDir);
-        int start_cluster = currentDir->DIR_FstClusLO;
-        int size = currentDir->DIR_FileSize;
+        char* name = process_name(current_entry->DIR_Name, isDir);
+        int start_cluster = current_entry->DIR_FstClusLO;
+        int size = current_entry->DIR_FileSize;
         
-        if (currentDir->DIR_Name[0] != 0xE5 && currentDir->DIR_Name[0] != 0x00){
+        if (current_entry->DIR_Name[0] != 0xE5 && current_entry->DIR_Name[0] != 0x00){
             printf("%s ", name);
             if (isDir){
                 printf("(starting cluster = %d)", start_cluster);
@@ -236,6 +249,183 @@ void list_root_directory(DirEntry *root_dir, int root_cluster, char* fat, char *
 
 
 
+
+// Milestone 4: recover a small file
+// If your nyufile program is invoked with option -r filename, it should recover the deleted file with the specified name. 
+// The workflow is better illustrated through an example:
+
+// [root@... cs202]# mount fat32.disk /mnt/disk
+// [root@... cs202]# ls -p /mnt/disk
+// DIR/  EMPTY  HELLO.TXT
+// [root@... cs202]# cat /mnt/disk/HELLO.TXT
+// Hello, world.
+// [root@... cs202]# rm /mnt/disk/HELLO.TXT
+// rm: remove regular file '/mnt/disk/HELLO.TXT'? y
+// [root@... cs202]# ls -p /mnt/disk
+// DIR/  EMPTY
+// [root@... cs202]# umount /mnt/disk
+// [root@... cs202]# ./nyufile fat32.disk -l
+// DIR/ (starting cluster = 4)
+// EMPTY (size = 0)
+// Total number of entries = 2
+// [root@... cs202]# ./nyufile fat32.disk -r HELLO
+// HELLO: file not found
+// [root@... cs202]# ./nyufile fat32.disk -r HELLO.TXT
+// HELLO.TXT: successfully recovered
+// [root@... cs202]# ./nyufile fat32.disk -l
+// HELLO.TXT (size = 14, starting cluster = 3)
+// DIR/ (starting cluster = 4)
+// EMPTY (size = 0)
+// Total number of entries = 3
+// [root@... cs202]# mount fat32.disk /mnt/disk
+// [root@... cs202]# ls -p /mnt/disk
+// DIR/  EMPTY  HELLO.TXT
+// [root@... cs202]# cat /mnt/disk/HELLO.TXT
+// Hello, world.
+
+// For all milestones, you only need to recover regular files (including empty files, but not directory files) in the root directory. 
+// When the file is successfully recovered, your program should print filename: successfully recovered (replace filename with the actual file name).
+
+// For all milestones, you can assume that no other files or directories are created or modified since the deletion of the target file. 
+// However, multiple files may be deleted.
+
+// Besides, for all milestones, you don’t need to update the FSINFO structure because most operating systems don’t care about it.
+
+// Here are a few assumptions specifically for Milestone 4:
+
+// The size of the deleted file is no more than the size of a cluster.
+// At most one deleted directory entry matches the given filename. 
+
+// If no such entry exists, your program should print filename: file not found (replace filename with the actual file name).
+
+char* padding_filename(char* filename){
+    char* padded_name = malloc(20);  //we don't need to save memory right?
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < 8; i++){
+        // printf("File[%d] %c ",j,  filename[j]);
+        if (filename[j] == '.'){
+            padded_name[i] = ' ';
+        } else if (filename[j] == '\0'){
+            break;
+        } else {
+            padded_name[i] = filename[j];
+            j++;
+        }
+        // printf("PAD[%d] %c \n",i,  padded_name[i]);
+    }
+    for (;i < 11; i++){
+        if (i<8){
+            padded_name[i] = ' ';
+            continue;
+        }
+        // printf("File[%d] %c ",j,  filename[j]);
+        if (filename[j] == '.'){
+            j++;
+        }
+        if (filename[j] == '\0'){
+            padded_name[i] = ' ';
+        } else {
+            padded_name[i] = filename[j];
+            j++;
+        }
+        // printf("PAD[%d] %c\n",i,  padded_name[i]);
+    }
+    padded_name[11] = '\0';
+    //print the padded name char by char
+    // printf("Padded name: \n");
+    // for (i = 0; i < 11; i++){
+    //     printf("PAD[%d] %c\n",i,  padded_name[i]);
+    // }
+    return padded_name;
+}
+
+DirEntry* search_for_deleled_file(DirEntry *root_dir, char* targetfile, int root_cluster, char* fat, char * data_region, int bytes_per_sector, int sectors_per_cluster, int* mult){
+    * mult = 0;
+
+    int i = 0;
+
+    size_t size_dir = sizeof(DirEntry);
+    int current_cluster = root_cluster;
+
+    int number_entries_per_cluster = bytes_per_sector * sectors_per_cluster / size_dir;
+    // printf("number_entries_per_cluster = %d\n\n", number_entries_per_cluster);
+    
+    // print_bytes((char *)root_dir, 32);
+
+    // printf("Searching for file %s\n", targetfile);
+
+    DirEntry* found_file = NULL;
+
+    while (root_dir[i].DIR_Name[0] != 0x00 || get_next_cluster(fat, current_cluster) != -1){
+
+        DirEntry* current_entry = &(root_dir[i]);
+
+        int isDir = current_entry->DIR_Attr & 0x10;
+
+        char* name = process_name(current_entry->DIR_Name, isDir);
+        int start_cluster = current_entry->DIR_FstClusLO;
+        int size = current_entry->DIR_FileSize;
+
+        // printf("Checking file %s\n", name);
+        // printf("Target: %s\n", targetfile);
+        
+        if (current_entry->DIR_Name[0] == 0xE5 && strcmp((targetfile+1), (name+1)) == 0){
+            if (found_file != NULL){
+                // printf("Multiple files\n");
+                *mult = 1;
+                return NULL;
+            }
+            found_file = current_entry;
+            // printf("found file! %s\n", name);
+        }
+
+        i++;
+
+        if (i % number_entries_per_cluster == 0){
+            int next_cluster = get_next_cluster(fat, current_cluster);
+            if (next_cluster == -1){
+                break;
+            } else {
+                current_cluster = next_cluster;
+                root_dir = (DirEntry *)get_cluster_address(data_region, current_cluster, bytes_per_sector, sectors_per_cluster);
+                i = 0;
+            }
+        }
+    }
+    // if (found_file == NULL){
+    //     printf("File not found\n");
+    // }
+    return found_file;
+}
+
+// Milestone 5: recover a large contiguously-allocated file
+
+// Now, you will recover a file that is larger than one cluster. 
+// Nevertheless, for Milestone 5, you can assume that such a file is allocated contiguously. 
+// You can continue to assume that at most one deleted directory entry matches the given filename. 
+// If no such entry exists, your program should print filename: file not found (replace filename with the actual file name).
+
+// Milestone 6: detect ambiguous file recovery requests
+// In Milestones 4 and 5, you assumed that at most one deleted directory entry matches the given filename. 
+// However, multiple files whose names differ only in the first character would end up having the same name when deleted. 
+// Therefore, you may encounter more than one deleted directory entry matching the given filename. 
+// When that happens, your program should print filename: multiple candidates found (replace filename with the actual file name) and abort.
+
+// This scenario is illustrated in the following example:
+
+// [root@... cs202]# mount fat32.disk /mnt/disk
+// [root@... cs202]# echo "My last name is Tang." > /mnt/disk/TANG.TXT
+// [root@... cs202]# echo "My first name is Yang." > /mnt/disk/YANG.TXT
+// [root@... cs202]# sync
+// [root@... cs202]# rm /mnt/disk/TANG.TXT /mnt/disk/YANG.TXT
+// rm: remove regular file '/mnt/disk/TANG.TXT'? y
+// rm: remove regular file '/mnt/disk/YANG.TXT'? y
+// [root@... cs202]# umount /mnt/disk
+// [root@... cs202]# ./nyufile fat32.disk -r TANG.TXT
+// TANG.TXT: multiple candidates found
+
+
 int main(int argc, char **argv) {
     int opt;
     int iFlag = 0;
@@ -243,9 +433,9 @@ int main(int argc, char **argv) {
     int rFlag = 0;
     int RFlag = 0;
     int sFlag = 0;
-    char *disk_filename;
-    char *filename;
-    char *sha1;
+    char *disk_filename = NULL;
+    char *filename = NULL;
+    char *sha1 = NULL;
     int badArgs = 1;
 
     if (argc < 2) {
@@ -267,11 +457,13 @@ int main(int argc, char **argv) {
                 badArgs = 0;
                 rFlag = 1;
                 filename = optarg;
+                // printf("r: %s\n", filename);
                 break;
             case 'R':
                 badArgs = 0;
                 RFlag += 1;
                 filename = optarg;
+                // printf("R: %s\n", filename);
                 break;
             case 's':
                 badArgs = 0;
@@ -280,6 +472,7 @@ int main(int argc, char **argv) {
                 break;
             case '?':
                 badArgs = 1;
+                // printf("badArgs\n");
                 break;
             default:
                 print_usage();
@@ -288,6 +481,7 @@ int main(int argc, char **argv) {
     }
     // Check that the disk filename was provided
     if (optind >= argc) {
+        // printf("optind >= argc\n");
         print_usage();
     } else {
         disk_filename = argv[optind];
@@ -297,7 +491,7 @@ int main(int argc, char **argv) {
     if (iFlag + lFlag + rFlag + RFlag > 1) {
         print_usage();
     }
-    if (rFlag + RFlag > 0 && sFlag == 0) {
+    if (RFlag == 1 && sFlag == 0) {
         print_usage();
     }
     if (iFlag + lFlag > 0 && sFlag == 1) {
@@ -364,17 +558,85 @@ int main(int argc, char **argv) {
     // printf("get next cluster of 3 %d\n", get_next_cluster(fat1, 3));
     // printf("get next cluster of 4 %d\n", get_next_cluster(fat1, 4));
 
-    if (iFlag) {
-        print_system_info(numFats, bytesPerSector, sectorsPerCluster, reservedSectors);
-    } else if (lFlag) {
-        // display root directory
+
+    // display root directory
         char *rootDirAddr = mapped + rootDirStartByte;
         // printf("%p\n", rootDirAddr);
         // printf("%p\n", get_cluster_address(dataRegion, rootDirCluster, bytesPerSector, sectorsPerCluster));
 
-        // // convert the content of root directory to file entries
-        list_root_directory((DirEntry *)rootDirAddr, rootDirCluster, fat1, dataRegion, bytesPerSector, sectorsPerCluster);
 
+    if (iFlag) {
+        print_system_info(numFats, bytesPerSector, sectorsPerCluster, reservedSectors);
+    } else if (lFlag) {
+        list_root_directory((DirEntry *)rootDirAddr, rootDirCluster, fat1, dataRegion, bytesPerSector, sectorsPerCluster);
+    } else if (rFlag && !sFlag) {
+        // printf("Recovering contiguous file %s without SHA\n", filename);
+        
+        DirEntry *fileDir = NULL;
+        int mult = 0;
+
+        if (filename != NULL){
+            fileDir = search_for_deleled_file((DirEntry *)rootDirAddr, filename, rootDirCluster, fat1, dataRegion, bytesPerSector, sectorsPerCluster, &mult);
+        }
+        
+        if (mult == 1){
+            printf("%s: multiple candidates found\n", filename);
+        } else if (fileDir != NULL){
+            char *name = fileDir->DIR_Name;
+            // printf("Found file %s\n", padding_filename(name));
+
+            int size = fileDir->DIR_FileSize;
+            unsigned short FstClusLO = fileDir->DIR_FstClusLO;
+            unsigned short FstClusHI = fileDir->DIR_FstClusHI;
+            int firstCluster = FstClusLO + (FstClusHI << 16);
+            // printf("Cluster start: %d\n", firstCluster);
+            // printf("Size: %d\n", size);
+            // printf("Start from byte %d\n", get_cluster_address(dataRegion, firstCluster, bytesPerSector, sectorsPerCluster) - mapped);
+
+            int fileClusterNum = (int)ceil((float)size / (float)(bytesPerSector * sectorsPerCluster));
+            // printf("Num of clusters in file: %d\n", fileClusterNum);
+
+            char* recoveredName = padding_filename(filename);
+            memcpy(fileDir->DIR_Name, recoveredName, 11);
+            // printf("Recovered name: [%s]\n", recoveredName);    
+        
+            if (fileClusterNum == 1){
+                //recover the fat
+                int* fileFatEntry = (int*)fat1 + firstCluster;
+                // print_bytes((char *)fileFatEntry, 4);
+                *fileFatEntry = 0x0FFFFFF8;
+                // print_bytes((char *)fileFatEntry, 4);
+
+
+                // I only handled FAT 1 !!!! BUT it passed the milestone 4 
+
+
+            } else {
+               
+                int fatID = 0;
+                for (fatID = 0; fatID < numFats; fatID++){
+                    char *currentFat = FATs[fatID];
+
+                    int* fileFatEntry = (int*)currentFat + firstCluster;
+                    // print_bytes((char *)fileFatEntry, 16);
+
+                    int i = 0;
+                    for (i = 0; i < fileClusterNum; i++){
+                        int *currentEntry = (int*)currentFat + (firstCluster+i);
+                        if (i != fileClusterNum-1){
+                            *currentEntry = firstCluster + i + 1;
+                        } else {
+                            *currentEntry = 0x0FFFFFF8;
+                        }
+                    }
+                    // print_bytes((char *)fileFatEntry, 16);
+                }
+
+            }
+            printf("%s: successfully recovered\n", filename);
+        } else {
+            printf("%s: file not found\n", filename);
+        }
     }
 
 } 
